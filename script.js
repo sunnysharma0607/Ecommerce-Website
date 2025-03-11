03509458f343221679c50edb35faf26a79799bd2
Sunny Sharma <sunnysharma0706@gmail.com>
2025-03-12 02:44:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let card = document.querySelectorAll(".trend");
+let blog = document.querySelectorAll(".trends")
+let mainPage = document.querySelectorAll(".main");
+function shops(){
+    mainPage.style.display="none";
+    blog.style.display="none";
+
+    // card.style.display="none";
+
+
+}
